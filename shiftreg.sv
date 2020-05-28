804ecc3910a428783068c1c123dbfafc907a4280
Magmatic <sbrocketman80@gmail.com>
2020-05-27 21:13:06 -0700

    Added whitespace to shiftreg

--- shiftreg.sv
+++ shiftreg.sv
@@ -1,18 +1,15 @@
 module shiftreg#(parameter N = 8)
-(input logic clk,
-input logic reset, load,
-input logic sin,
-input logic [N-1:0]d,
-output logic [N-1:0]q,
-output logic sout);
+                (input  logic clk, reset, load, sin,
+                 input  logic [N-1:0]d,
+                 output logic [N-1:0]q,
+                 output logic sout);
 
-always_ff@(posedge clk, posedge reset)
-begin
-if(reset) q<=0;
-else if(load) q <=d;
-else q <= {q[N-2:0], sin};
-end
+    always_ff @(posedge clk, posedge reset) begin
+        if(reset) q <=0;
+        else if(load) q <=d;
+        else q <= {q[N-2:0], sin};
+    end
 
-assign sout = q[N-1];
+    assign sout = q[N-1];
 endmodule 
 
