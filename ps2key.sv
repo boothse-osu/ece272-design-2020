edd7b732250a7b82ba583e97482e8ea245402cb2
Samuel <wagnsamu@oregonstate.edu>
2020-05-29 20:11:18 -0700

    test

--- ps2key.sv
+++ ps2key.sv
@@ -2,6 +2,10 @@ module ps2key#(parameter N = 11)
 (input logic [N-1:0]q, input logic sout, 
 output logic [3:0]numbers, output logic [2:0]directions);
 
+logic [N-1:0]q1;
+
+assign q1 = q;
+
 always_comb
 	case(q)
 	//start, reversed ps2code, parity, stop
@@ -24,7 +28,7 @@ always_comb
 	//9
 	
 	//0
-	default: numbers = 8'b1111
+	default: numbers = 11'b111111111111
 	
 	
 
